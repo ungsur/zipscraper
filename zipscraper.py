7dc9d2128cd251ddfe87238a0bb8989eb1b7ad93
Rich Ungsunan <ungsunan@gmail.com>
2017-01-01 15:39:55 -0500

    adding comments

--- zipscraper.py
+++ zipscraper.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#Regular expressions library is used for pattern matching
 import re
 import urllib
 
@@ -12,13 +13,21 @@ data = response.read()
 
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(data, "lxml")
+
+#this returns all of the parts of the page as soup elements
 data=soup.find_all()
 
-
+#this returns a list of the parts of the page which start the the part <script>
 script = soup.find_all('script')
+
+#turn the 3rd element on the page to one long string
 tempstring = str(script[3])
+
+#grab the part of the strings that starts with a ( has some numbers inbetween and ends with a )
+#['(-34.7736969, -58.7326622)', '(-34.7736969, -58.7326622)', '(-34.7736969, -58.7326622)']
 templist = re.findall('\(.[0-9]+.+[0-9]\)',tempstring)
 
+#grab the first pair of coordinates and cut out the parentheses parts with .strip
 latitude = float(re.split(",",templist[0])[0].strip("()"))
 longitude = float(re.split(",",templist[0])[1].strip("()"))
 
