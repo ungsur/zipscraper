c79ff022129d50078b84c4b9a2c9c5996ff982ec
Rich Ungsunan <ungsunan@gmail.com>
2017-01-02 14:00:07 -0500

    Adding comments

--- zipscraper.py
+++ zipscraper.py
@@ -2,36 +2,13 @@
 #Regular expressions library is used for pattern matching
 import re
 import urllib
+from bs4 import BeautifulSoup
 
+#user agent is so that the website thinks it's a computer, not a program getting the page
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 headers={'User-Agent':user_agent,} 
-url = "http://www.codigopostalde.com.ar/buenos-aires/20-de-junio/calle-casacuberta/00000002-00000100/"
-#url = ''
-request=urllib.request.Request(url,None,headers)
-response = urllib.request.urlopen(request)
-data = response.read()
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(data, "lxml")
-
-#this returns all of the parts of the page as soup elements
-data=soup.find_all()
-
-#this returns a list of the parts of the page which start the the part <script>
-script = soup.find_all('script')
-
-#turn the 3rd element on the page to one long string
-tempstring = str(script[3])
-
-#grab the part of the strings that starts with a ( has some numbers inbetween and ends with a )
-#['(-34.7736969, -58.7326622)', '(-34.7736969, -58.7326622)', '(-34.7736969, -58.7326622)']
-templist = re.findall('\(.[0-9]+.+[0-9]\)',tempstring)
-
-#grab the first pair of coordinates and cut out the parentheses parts with .strip
-latitude = float(re.split(",",templist[0])[0].strip("()"))
-longitude = float(re.split(",",templist[0])[1].strip("()"))
-
-
+#from the first link, turn the contents of the first page into a soup object
 url1 = 'http://www.codigopostalde.com.ar/en/'
 request1 =  urllib.request.Request(url1,None,headers)
 response1 =  urllib.request.urlopen(request1)
@@ -39,14 +16,23 @@ data = response1.read()
 
 soup = BeautifulSoup(data, "lxml")
 print(soup.prettify())
-urllist = soup.find('h3').find_next('ul')
 
+'''
+we see that the links are after the /h3 section, so we look for 
+the unordered lists after it and grab all of the links and put it in a 
+dictionary of states with the statename as the key and the link as the value
 
+'''
 states_dict = {}
 for link in soup.find('h3').find_next('ul').find_all("a"):
     print(link.text, link.get("href"))
     states_dict[link.text] = str(link.get("href"))
     
+'''
+From the list of states/links, we make a BS object from each state page to 
+make a dictionary with city name as the key and city url, zipcode, and lat/long
+as the values
+'''
 city_dict = {}
 for k,v in states_dict.items():
     url_state = v
@@ -58,17 +44,34 @@ for k,v in states_dict.items():
     for link in temp_soup.find('h3').find_next('ul').find_all("a"):
         city_dict[link.text] = {'url':str(link.get("href")),'zipcode':'','lat':0.0,'long':0.0}
 
-
+'''
+We go over city dictionary again and fill in the zip code and lat/long values.
+Some cities don't have any lat/long data, so we have to use try/exception to
+skip over any errors. The counter is just to see how far the progam is going.
+There are 15706 city pages.
+'''
 counter = 0 
 for k in city_dict.keys():
+    #this part fills in the zipcode
     zipurl = city_dict[k]['url']
     ziprequest=urllib.request.Request(zipurl,None,headers)
     zipresponse = urllib.request.urlopen(ziprequest)
     zipdata = zipresponse.read()
     zipsoup = BeautifulSoup(zipdata, "lxml")
     print(zipurl)
+    
+    '''
+    the part between the ' '  is the regular expressions search. From the 
+    whole zipstring, we go to the h1 section, then look for the 2nd instance
+    of 'strong' after that:
+        zipstring = <strong>Postal Code 4644</strong>
+    '''
     zipstring = str(zipsoup.find('h1').find_next('strong').find_next('strong'))
+    
+    #To get zipcode from zipstring, we match only the numbers in the string
     zipcode = re.findall('[0-9]+.+[0-9]', zipstring)
+    
+    #this part fills in the lat/long
     script = zipsoup.find_all('script')
     counter = counter+1
     print(counter)
@@ -79,6 +82,7 @@ for k in city_dict.keys():
         longitude = float(re.split(",",templist[0])[1].strip("()"))
     except:
         pass
+    
     city_dict[k]['zipcode']=zipcode
     city_dict[k]['lat']=latitude
     city_dict[k]['long']=longitude
